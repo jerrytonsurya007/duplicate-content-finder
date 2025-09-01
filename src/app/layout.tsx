63e39f25410cac9ec86f42d6b41faa15f19caa64
Jerryton Surya <jerrytonsurya@novactech.in>
2025-09-01 08:45:48 +0000

    Integrate with console.firebase
    FIREBASE_API_KEY=AIzaSyAy-DAYckN35DFn4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { cn } from '@/lib/utils';
+import "@/lib/firebase";
 
 export const metadata: Metadata = {
   title: 'Article Insights',
